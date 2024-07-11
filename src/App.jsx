1c80f1606f854594f034a2f36b18c338b4de0ae8
Ahmed-kotb621 <akotb621@gmail.com>
2024-07-11 15:07:06 +0300

    initial

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+import logo from "./assets/logo.png";
+
+function App() {
+  return (
+    <div>
+      <div className="main-landing">
+        <img src={logo} alt="meseallah logo" />
+      </div>
+      <div className="coming">
+        <p>قريباً</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
